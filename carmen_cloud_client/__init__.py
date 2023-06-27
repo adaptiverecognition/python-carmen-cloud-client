7057a85c81c9ff6a21e36cf252899f375980b0ae
Botond BALÁZS <me@botond.online>
2023-06-27 14:07:24 +0200

    Unified library initial commit

---
+++ carmen_cloud_client/__init__.py
@@ -0,0 +1,4 @@
+from .transport import  TransportAPIClient, TransportationCargoApiResponse, TransportAPIOptions, CodeType
+from .errors import CarmenAPIConfigError
+
+__version__ = "1.0.0"
---
+++ carmen_cloud_client/errors.py
@@ -0,0 +1,13 @@
+class CarmenAPIConfigError(Exception):
+    """
+    Exception raised when the provided options for TransportAPIClient are invalid.
+
+    Args:
+        message (str): The error message.
+
+    Attributes:
+        name (str): The name of the exception.
+    """
+    def __init__(self, message):
+        super().__init__(message)
+        self.name = "TransportAPIConfigError"
