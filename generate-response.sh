efaedcbef0a86901fd1d4fe221d86c34d5d34edc
Botond BALÁZS <me@botond.online>
2024-05-14 13:14:39 +0200

    Descriptor API schemas and models

--- generate-response.sh
+++ generate-response.sh
@@ -11,3 +11,13 @@ datamodel-codegen --input-file-type jsonschema --input assets/storage-and-hook/H
 datamodel-codegen --input-file-type jsonschema --input assets/storage-and-hook/OKResponse.schema.json --output carmen_cloud_client/storage_and_hook/ok_response.py
 datamodel-codegen --input-file-type jsonschema --input assets/storage-and-hook/StorageStatusResponse.schema.json --output carmen_cloud_client/storage_and_hook/storage_status_response.py
 datamodel-codegen --input-file-type jsonschema --input assets/storage-and-hook/UpdateHookRequest.schema.json --output carmen_cloud_client/storage_and_hook/update_hook_request.py
+
+datamodel-codegen --input-file-type jsonschema --input assets/descriptor/APIListResponse.schema.json --output carmen_cloud_client/descriptor/api-list-response.py
+datamodel-codegen --input-file-type jsonschema --input assets/descriptor/DimensionListReponse.schema.json --output carmen_cloud_client/descriptor/dimension-list-response.py
+datamodel-codegen --input-file-type jsonschema --input assets/descriptor/PaidSubscriptionListResponse.schema.json --output carmen_cloud_client/descriptor/paid-subscription-list-response.py
+datamodel-codegen --input-file-type jsonschema --input assets/descriptor/PaidSubscriptionUsageResponse.schema.json --output carmen_cloud_client/descriptor/paid-subscription-usage-response.py
+datamodel-codegen --input-file-type jsonschema --input assets/descriptor/ProductsResponse.schema.json --output carmen_cloud_client/descriptor/products-response.py
+datamodel-codegen --input-file-type jsonschema --input assets/descriptor/UsagePlanListResponse.schema.json --output carmen_cloud_client/descriptor/usage-plan-list-response.py
+datamodel-codegen --input-file-type jsonschema --input assets/descriptor/UsagePlanSubscriptionListResponse.schema.json --output carmen_cloud_client/descriptor/usage-plan-subscription-list-response.py
+datamodel-codegen --input-file-type jsonschema --input assets/descriptor/Prices.schema.json --output carmen_cloud_client/descriptor/prices.py
+datamodel-codegen --input-file-type jsonschema --input assets/descriptor/UsagePlanUsageResponse.schema.json --output carmen_cloud_client/descriptor/usage-plan-usage-response.py
--- update-openapi-spec.sh
+++ update-openapi-spec.sh
@@ -12,6 +12,16 @@ aws apigateway get-model --rest-api-id 24ut7ue8d8 --model-name OKResponse | jq -
 aws apigateway get-model --rest-api-id 24ut7ue8d8 --model-name StorageStatusResponse | jq --raw-output '.schema' > ./assets/storage-and-hook/StorageStatusResponse.schema.json
 aws apigateway get-model --rest-api-id 24ut7ue8d8 --model-name UpdateHookRequest | jq --raw-output '.schema' > ./assets/storage-and-hook/UpdateHookRequest.schema.json
 
+aws apigateway get-model --rest-api-id yqh6c9omp7 --model-name APIListResponse | jq --raw-output '.schema' > ./assets/descriptor/APIListResponse.schema.json
+aws apigateway get-model --rest-api-id yqh6c9omp7 --model-name DimensionListReponse | jq --raw-output '.schema' > ./assets/descriptor/DimensionListReponse.schema.json
+aws apigateway get-model --rest-api-id yqh6c9omp7 --model-name PaidSubscriptionListResponse | jq --raw-output '.schema' > ./assets/descriptor/PaidSubscriptionListResponse.schema.json
+aws apigateway get-model --rest-api-id yqh6c9omp7 --model-name PaidSubscriptionUsageResponse | jq --raw-output '.schema' > ./assets/descriptor/PaidSubscriptionUsageResponse.schema.json
+aws apigateway get-model --rest-api-id yqh6c9omp7 --model-name ProductsResponse | jq --raw-output '.schema' > ./assets/descriptor/ProductsResponse.schema.json
+aws apigateway get-model --rest-api-id yqh6c9omp7 --model-name UsagePlanListResponse | jq --raw-output '.schema' > ./assets/descriptor/UsagePlanListResponse.schema.json
+aws apigateway get-model --rest-api-id yqh6c9omp7 --model-name UsagePlanSubscriptionListResponse | jq --raw-output '.schema' > ./assets/descriptor/UsagePlanSubscriptionListResponse.schema.json
+aws apigateway get-model --rest-api-id yqh6c9omp7 --model-name Prices | jq --raw-output '.schema' > ./assets/descriptor/Prices.schema.json
+aws apigateway get-model --rest-api-id yqh6c9omp7 --model-name UsagePlanUsageResponse | jq --raw-output '.schema' > ./assets/descriptor/UsagePlanUsageResponse.schema.json
+
 for file in $(find ./assets/storage-and-hook -name '*.json'); do
     # replace the remote reference with the local one
     sed -i 's|https://apigateway.amazonaws.com/restapis/'"24ut7ue8d8"'/models/\(.*\)"|\1.schema.json"|g' "$file"
